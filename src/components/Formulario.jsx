b5a7035ab432e856dd50a84c505fba5d89c93175
Albasan08 <sandin.alba@gmail.com>
2025-12-14 12:11:40 +0100

    Add: useFormulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,21 +1,10 @@
+import { useFormulario } from '../hooks/useFormulario'
 // único objetivo del formulario: capturar el valor del input, la categoría
-// Pasarle los "argumentos" desde el padre (Galeria), sino no los reconoce el padre
-import { useState } from "react";
 export const Formulario = ({ categoriaNueva }) => {
     // No hace falta añadir los estados ya que se lo pasamos directamente a la Galería por categoriaNueva(categoria)
     //const [categoria, setCategoria] = useState('');
 
-    // Recoge el valor del input buscado
-    const handleRecogerCategoria = (ev) => {
-        ev.preventDefault()
-        //console.log(ev.target, 'desde formulario')
-
-        // para capturar la categoria hay que poner el name del input.value
-        let categoria = ev.target.categoria.value;
-        // No hace falta, se envía directamente
-        //setCategoria(categoria);
-        categoriaNueva(categoria);
-    };
+    const { handleRecogerCategoria } = useFormulario(categoriaNueva);
 
     return (
         <>
